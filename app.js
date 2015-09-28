ee3ca013216a558e6330dac48fc705d0e7d8bff4
CorcovadoMing <rf37535@gmail.com>
2015-09-29 07:46:24 +0800

    Add morgan logger

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-os = require('os');
-app = require('express.io')();
-express = require('express');
+var os = require('os');
+var app = require('express.io')();
+var express = require('express');
+var morgan = require('morgan');
+
+var port = 7076;
 app.http().io();
 
 app.io.route('ready', function(req) {
@@ -9,13 +12,14 @@ app.io.route('ready', function(req) {
         cpus: JSON.stringify(os.cpus()),
 	freemem: JSON.stringify(os.freemem()),
 	totalmem: JSON.stringify(os.totalmem())
-    })
-})
+    });
+});
 
+app.use(morgan('combined'));
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html')
-})
+    res.sendfile(__dirname + '/index.html');
+});
 
-app.listen(7076)
+app.listen(port);
